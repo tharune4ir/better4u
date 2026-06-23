025a272a348b409bb2fd0ae38a65d8ddb54810b3
Tharun Gajula <tharun.gajula.2@gmail.com>
2026-06-23 16:19:16 +0530

    feat: restore first-layer global lock screen using rudra22 passcode without hints

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LockScreen from "@/components/LockScreen";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -53,7 +54,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[#F7F6F2]">
-        {children}
+        <LockScreen>
+          {children}
+        </LockScreen>
       </body>
     </html>
   );
